bdbfcc1ce362a77d88ac4f6756745463bb5131dc
sean <sean@isanglyeol-ui-MacBookPro.local>
2020-04-03 12:20:39 +0900

    Updates

---
+++ precache-manifest.4ff563e628c7fe028e7da0778bda54e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed01138d91953dcefc217bdfb38dbdc1",
+    "url": "/putshong/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9778387d91990213aaa2",
+    "url": "/putshong/movie_app_2020/static/css/main.cca5437f.chunk.css"
+  },
+  {
+    "revision": "cb95dd1f9fbdc9813780",
+    "url": "/putshong/movie_app_2020/static/js/2.c9fd2d69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/putshong/movie_app_2020/static/js/2.c9fd2d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9778387d91990213aaa2",
+    "url": "/putshong/movie_app_2020/static/js/main.3f7b48da.chunk.js"
+  },
+  {
+    "revision": "96719893d3d37a872590",
+    "url": "/putshong/movie_app_2020/static/js/runtime-main.f888ea4e.js"
+  }
+]);
